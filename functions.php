f8d83aacb44a585ad0e305c665e552485a8b03d7
Cuong Dinh Lai <contact@sauhi.com>
2014-08-21 12:41:12 +0700

    No need to run load variable

--- functions.php
+++ functions.php
@@ -2,8 +2,6 @@
 $sb_load = get_template_directory() . "/sb/load.php";
 if(file_exists($sb_load)) {
 	include $sb_load;
-	$load = new SB_Load();
-	$load->run();
 } else {
 	wp_die('Please put sb folder in your theme.');
 }
