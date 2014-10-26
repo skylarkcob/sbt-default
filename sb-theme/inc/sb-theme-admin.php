bdbfa2fd13dec16cf9f0655e31c42d2c592d4957
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2014-10-26 11:47:09 +0700

    Support coupon theme.

--- sb-theme/inc/sb-theme-admin.php
+++ sb-theme/inc/sb-theme-admin.php
@@ -14,6 +14,7 @@ function sb_theme_setting_field() {
     SB_Admin_Custom::add_section('sb_theme_setting_section', __('SB Theme options page', 'sbteam'), 'sb_theme');
     SB_Admin_Custom::add_setting_field('sb_theme_logo', 'Logo', 'sb_theme_setting_section', 'sb_theme_logo_callback', 'sb_theme');
     SB_Admin_Custom::add_setting_field('sb_theme_favicon', 'Favicon', 'sb_theme_setting_section', 'sb_theme_favicon_callback', 'sb_theme');
+    sb_theme_setting_field_no_thumbnail();
     do_action('sb_theme_setting_field');
 }
 add_action('sb_admin_init', 'sb_theme_setting_field');
@@ -21,11 +22,23 @@ add_action('sb_admin_init', 'sb_theme_setting_field');
 function sb_theme_logo_callback() {
     $options = SB_Option::get();
     $value = isset($options['theme']['logo']) ? $options['theme']['logo'] : '';
-    SB_Field::media_image('sb_theme_logo', 'sb_options[theme][logo]', $value, __('You can enter url or upload new logo image file.', 'sb-theme'));
+    $args = array(
+        'id' => 'sb_theme_logo',
+        'name' => 'sb_options[theme][logo]',
+        'value' => $value,
+        'description' => __('You can enter url or upload new logo image file.', 'sb-theme')
+    );
+    SB_Field::media_image($args);
 }
 
 function sb_theme_favicon_callback() {
     $options = SB_Option::get();
     $value = isset($options['theme']['favicon']) ? $options['theme']['favicon'] : '';
-    SB_Field::media_image('sb_theme_favicon', 'sb_options[theme][favicon]', $value, __('You can enter url or upload new favicon image file.', 'sb-theme'));
+    $args = array(
+        'id' => 'sb_theme_favicon',
+        'name' => 'sb_options[theme][favicon]',
+        'value' => $value,
+        'description' => __('You can enter url or upload new favicon image file.', 'sb-theme')
+    );
+    SB_Field::media_image($args);
 }
