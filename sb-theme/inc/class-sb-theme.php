fda95da823c02d95a837a0646329ff7d1a1cbb36
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2014-11-23 21:49:21 +0700

    Update new theme core 1.0.9

--- sb-theme/inc/class-sb-theme.php
+++ sb-theme/inc/class-sb-theme.php
@@ -18,6 +18,12 @@ class SB_Theme {
         }
     }
 
+    public static function sidebar($name) {
+        if(!dynamic_sidebar($name)) {
+            dynamic_sidebar('primary');
+        }
+    }
+
     public static function the_favicon_html() {
         $favicon_url = SB_Option::get_favicon_url();
         if(!empty($favicon_url)) {
@@ -30,6 +36,50 @@ class SB_Theme {
             printf('<meta content="%s" property="article:published_time">', get_the_date('c'));
             printf('<meta content="%s" property="article:modified_time">', get_the_modified_time('c'));
         }
+    }
+
+    public static function the_paginate($args = array()) {
+        if(function_exists('sb_paginate')) {
+            sb_paginate($args);
+        }
+    }
+
+    public static function the_archive_title() {
+        if(is_category()) {
+            single_cat_title();
+        } elseif(is_tag()) {
+            single_tag_title();
+        } elseif(is_author()) {
+            the_author();
+        } elseif(is_day()) {
+            printf( __( 'Lưu trữ ngày %s' ), get_the_date() );
+        } elseif(is_month()) {
+            printf( __( 'Lưu trữ tháng %s' ), get_the_date('F Y') );
+        } elseif(is_year()) {
+            printf( __( 'Lưu trữ năm %s' ), get_the_date('Y') );
+        } else {
+            _e('Lưu trữ', 'sb-theme');
+        }
+    }
+
+    public static function the_comment_template() {
+        if(function_exists('sb_comment_template')) {
+            sb_comment_template();
+        }
+    }
+
+    public static function the_comments() {
+        if(function_exists('sb_comments')) {
+            sb_comments();
+        }
+    }
+
+    public static function get_text($en, $vi) {
+        return sprintf(__('<!--:en-->%1$s<!--:--><!--:vi-->%2$s<!--:-->'), $en, $vi);
+    }
+
+    public static function the_text($en, $vi) {
+        echo self::get_text($en, $vi);
     }
 
     public static function the_header() {
@@ -62,9 +112,54 @@ class SB_Theme {
     public static function the_menu($args = array()) {
         $superfish = isset($args['superfish']) ? (bool)$args['superfish'] : true;
         if($superfish) {
-            $args['menu_class'] = trim((isset($args['menu_class']) ? $args['menu_class'] : '') . ' sf-menu');
+            $menu_class = isset($args['menu_class']) ? $args['menu_class'] : '';
+            $menu_class = SB_PHP::add_string_with_space_before($menu_class, 'sf-menu');
+            $args['menu_class'] = $menu_class;
         }
-        wp_nav_menu($args);
+        $theme_location = isset($args['theme_location']) ? $args['theme_location'] : '';
+        $locations = SB_Core::get_menu_location();
+        if(empty($theme_location) || !array_key_exists($theme_location, $locations)) {
+            return;
+        }
+        $location_name = $locations[$theme_location];
+        switch($theme_location) {
+            case 'primary':
+                $location_name = 'Primary menu';
+                break;
+            case 'secondary':
+                $location_name = 'Secondary menu';
+                break;
+            case 'footer':
+                $location_name = 'Footer menu';
+                break;
+        }
+        $menu = wp_get_nav_menu_object($location_name);
+        if($menu && !is_wp_error($menu)) {
+            wp_nav_menu($args);
+        } else { ?>
+            <div class="sb-menu-container">
+                <ul class="<?php echo $args['menu_class']; ?>">
+                    <?php $default = isset($args['default']) ? $args['default'] : 'page';
+                    $posts_per_page = 8;
+                    if(isset($args['posts_per_page'])) {
+                        $posts_per_page = $args['posts_per_page'];
+                    }
+                    if('page' == $default) {
+                        $posts_per_page -= 2;
+                        $pages = SB_Query::get_pages(array('number' => $posts_per_page));
+                        foreach($pages as $page) : ?>
+                            <li class="menu-item"><a href="<?php echo get_permalink($page->ID); ?>"><?php echo $page->post_title; ?></a></li>
+                        <?php endforeach;
+                    } else {
+                        $taxonomy = isset($args['taxonomy']) ? $args['taxonomy'] : 'category';
+                        $terms = SB_Term::get($taxonomy, array('number' => $posts_per_page));
+                        foreach($terms as $term) : ?>
+                            <li class="menu-item"><a href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?></a></li>
+                        <?php endforeach;
+                    } ?>
+                </ul>
+            </div>
+        <?php }
     }
 
     public static function register_sidebar($sidebar_id, $sidebar_name, $sidebar_description) {
@@ -78,11 +173,98 @@ class SB_Theme {
         sb_theme_get_content('loading-dotted');
     }
 
+    public static function get_social_share_url($args = array()) {
+        $result = '';
+        $title = '';
+        $permalink = '';
+        $url = '';
+        $social_name = '';
+        $thumbnail = '';
+        $excerpt = '';
+        $language = SB_Option::get_default_language();
+        $twitter_account = '';
+        extract($args, EXTR_OVERWRITE);
+        if(empty($permalink)) {
+            $permalink = $url;
+        }
+        if(empty($twitter_account)) {
+            $twitter_account = SB_Option::get_theme_social('twitter');
+            $twitter_account = basename($twitter_account);
+        }
+        if(empty($twitter_account)) {
+            $twitter_account = 'skylarkcob';
+        }
+        switch($social_name) {
+            case 'facebook':
+                $url = 'https://www.facebook.com/sharer/sharer.php';
+                $url = add_query_arg('u', $permalink, $url);
+                $url = add_query_arg('t', $title, $url);
+                $result = $url;
+                break;
+            case 'googleplus':
+                $url = 'http://plusone.google.com/_/+1/confirm';
+                $url = add_query_arg('hl', $language, $url);
+                $url = add_query_arg('url', $permalink, $url);
+                $result = $url;
+                break;
+            case 'twitter':
+                $url = 'http://twitter.com/share';
+                $url = add_query_arg('url', $permalink, $url);
+                $url = add_query_arg('text', $title, $url);
+                $url = add_query_arg('via', $twitter_account, $url);
+                $result = $url;
+                break;
+            case 'pinterest':
+                $url = 'http://www.pinterest.com/pin/create/button';
+                $url = add_query_arg('media', $thumbnail, $url);
+                $url = add_query_arg('url', $permalink, $url);
+                $url = add_query_arg('description', $title . ' ' . $permalink, $url);
+                $result = $url;
+                break;
+            case 'zingme':
+                $url = 'http://link.apps.zing.vn/share';
+                $url = add_query_arg('t', $title, $url);
+                $url = add_query_arg('u', $permalink, $url);
+                $url = add_query_arg('desc', $excerpt, $url);
+                $result = $url;
+                break;
+        }
+        return $result;
+    }
+
     public static function get_page_not_found_image_url() {
         if(!class_exists('SB_Core')) {
             return;
         }
         return SB_CORE_URL . '/images/page-not-found.png';
+    }
+
+    public static function set_carousel_argument($args = array()) {
+        global $sb_carousel_argument;
+        $sb_carousel_argument = $args;
+    }
+
+    public static function get_carousel_argument() {
+        global $sb_carousel_argument;
+        if(!is_array($sb_carousel_argument)) {
+            $sb_carousel_argument = array();
+        }
+        return $sb_carousel_argument;
+    }
+
+    public static function set_nav_pills_argument($args = array()) {
+        global $sb_nav_pills_argument;
+        $sb_nav_pills_argument = $args;
+    }
+
+    public static function get_nav_pills_argument() {
+        global $sb_nav_pills_argument;
+        return $sb_nav_pills_argument;
+    }
+
+    public static function nav_pills($args = array()) {
+        self::set_nav_pills_argument($args);
+        sb_theme_get_content('nav-pills');
     }
 
     public static function set_modal_argument($args = array()) {
@@ -101,6 +283,26 @@ class SB_Theme {
     public static function modal($args = array()) {
         self::set_modal_argument($args);
         sb_theme_get_content('modal');
+    }
+
+    public static function carousel($args = array()) {
+        self::set_carousel_argument($args);
+        sb_theme_get_content('carousel');
+    }
+
+    public static function set_search_form_args($args = array()) {
+        global $sb_search_form_args;
+        $sb_search_form_args = $args;
+    }
+
+    public static function get_search_form_args() {
+        global $sb_search_form_args;
+        return $sb_search_form_args;
+    }
+
+    public static function the_search_form($args = array()) {
+        self::set_search_form_args($args);
+        sb_theme_get_content('search-form');
     }
 
     public static function get_loading_image($url = '') {
--- sb-theme/inc/sb-theme-functions.php
+++ sb-theme/inc/sb-theme-functions.php
@@ -47,6 +47,8 @@ function sb_theme_after_switch() {
 	if(!sb_theme_check_core()) {
 		wp_die(sprintf(__('You must install and activate plugin %1$s first! Click here to %2$s.', 'sb-theme'), '<a href="https://wordpress.org/plugins/sb-core/">SB Core</a>', sprintf('<a href="%1$s">%2$s</a>', admin_url('themes.php'), __('go back', 'sb-theme'))));
 	}
+    sb_theme_change_default_image_setting();
+    SB_Core::regenerate_htaccess_file();
 }
 add_action('after_switch_theme', 'sb_theme_after_switch');
 
@@ -54,22 +56,59 @@ if(!sb_theme_check_core()) {
     return;
 }
 
+if(!isset($content_width)) {
+    $content_width = 800;
+}
+
+function sb_theme_body_class($classes) {
+    $name = 'browser-' . SB_Browser::get_name();
+    $classes[] = $name;
+    if(SB_Detect::is_mobile()) {
+        $classes[] = 'mobile';
+    } else {
+        $classes[] = 'pc';
+    }
+    $classes[] = 'sb-theme sb-team';
+    return $classes;
+}
+add_filter('body_class', 'sb_theme_body_class');
+
+function sb_theme_post_class($classes) {
+    $classes[] = 'sb-post';
+    return $classes;
+}
+add_filter('post_class', 'sb_theme_post_class');
+
 function sb_theme_after_setup() {
     load_theme_textdomain('sb-theme', get_template_directory() . '/languages');
-
-    register_nav_menus(array(
-        'primary'   => __('Primary menu', 'sb-theme'),
-        'secondary' => __('Secondary menu', 'sb-theme'),
-        'footer' => __('Footer menu', 'sb-theme')
-    ));
-
+    register_nav_menus(
+        array(
+            'primary'   => __('Primary menu', 'sb-theme'),
+            'secondary' => __('Secondary menu', 'sb-theme'),
+            'footer' => __('Footer menu', 'sb-theme')
+        )
+    );
     add_theme_support('post-thumbnails');
-
-    add_theme_support('html5', array(
-        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-    ));
+    add_theme_support(
+        'html5',
+        array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption'
+        )
+    );
 }
 add_action('after_setup_theme', 'sb_theme_after_setup');
+
+function sb_theme_remove_default_image_size($sizes) {
+    unset($sizes['thumbnail']);
+    unset($sizes['medium']);
+    unset($sizes['large']);
+    return $sizes;
+}
+add_filter('intermediate_image_sizes_advanced', 'sb_theme_remove_default_image_size');
 
 function sb_theme_register_sidebar($sidebar_id, $sidebar_name, $sidebar_description) {
     SB_Theme::register_sidebar($sidebar_id, $sidebar_name, $sidebar_description);
@@ -104,17 +143,45 @@ function sb_get_custom_content($slug, $name = null) {
     sb_get_custom_template_part('content/' . $slug, $name);
 }
 
+function sb_get_custom_content_default_query($slug, $name = null) {
+    while(have_posts()) {
+        the_post();
+        sb_get_custom_content($slug);
+    }
+}
+
 function sb_get_custom_ajax($slug, $name = null) {
     sb_get_custom_template_part('ajax/' . $slug, $name);
+}
+
+function sb_get_custom_module($slug, $name = null) {
+    sb_get_custom_template_part('module/' . $slug, $name);
 }
 
 function sb_get_custom_carousel($slug, $name = null) {
     sb_get_custom_template_part('carousel/' . $slug, $name);
 }
 
+function sb_theme_add_term_meta($args = array()) {
+    if(!class_exists('SB_Term_Meta')) {
+        require SB_CORE_INC_PATH . '/class-sb-term-meta.php';
+    }
+    $term_meta = new SB_Term_Meta($args);
+}
+
+function sb_theme_term_meta_field_term_select($args = array()) {
+    if(!class_exists('SB_Term_Field')) {
+        require SB_CORE_INC_PATH . '/class-sb-term-field.php';
+    }
+    SB_Term_Field::term_select($args);
+}
+
 function sb_theme_style_and_script() {
     wp_enqueue_style('sb-theme-style', SB_THEME_URL . '/css/sb-theme-style.css');
     wp_enqueue_script('sb-theme', SB_THEME_URL . '/js/sb-theme-script.js', array('jquery'), false, true);
+    if(SB_Option::utility_enabled('jquery_marquee')) {
+        wp_enqueue_script('jquery-marquee', SB_THEME_LIB_URL . '/jquery-marquee/jquery.marquee.min.js', array('jquery'), false, true);
+    }
 }
 add_action('wp_enqueue_scripts', 'sb_theme_style_and_script');
 
@@ -134,6 +201,10 @@ add_filter('excerpt_more', 'sb_theme_excerpt_more');
 
 function sb_theme_add_setting_field($field_id, $field_title, $callback) {
     SB_Admin_Custom::add_setting_field($field_id, $field_title, 'sb_theme_setting_section', $callback, 'sb_theme');
+}
+
+function sb_theme_add_utilities_setting_field($field_id, $field_title, $callback) {
+    SB_Admin_Custom::add_setting_field($field_id, $field_title, 'sb_utilities_section', $callback, 'sb_utilities');
 }
 
 function sb_theme_the_logo() {
@@ -200,5 +271,65 @@ function sb_theme_check_support($name) {
     }
     return false;
 }
+
+function sb_theme_the_post_thumbnail($args = array()) {
+    $thumbnail_url = SB_Post::get_thumbnail_url($args);
+    $width = isset($args['width']) ? intval($args['width']) : 0;
+    $height = isset($args['height']) ? intval($args['height']) : 0;
+    if($width < 16) {
+        $width = $height;
+    }
+    if($height < 16) {
+        $height = $width;
+    }
+    if($width > 15) {
+        $crop = isset($args['crop']) ? $args['crop'] : false;
+        $crop = (bool)$crop;
+        $defaults = array(
+            'width' => $width,
+            'height' => $height,
+            'crop' => $crop
+        );
+        $params = isset($args['params']) ? $args['params'] : array();
+        $params = (array)$params;
+        $params = wp_parse_args($params, $defaults);
+        $thumbnail_url = bfi_thumb($thumbnail_url, $params);
+    }
+    $args['thumbnail_url'] = $thumbnail_url;
+    SB_Post::the_thumbnail_html($args);
+}
+
+function sb_theme_change_default_image_setting() {
+    update_option('image_default_align', 'center');
+    update_option('image_default_link_type', 'none');
+    update_option('image_default_size', 'large');
+}
+
+function sb_theme_get_custom_image_url($name) {
+    return SB_THEME_CUSTOM_URL . '/images/' . $name;
+}
+
+function sb_theme_paginate($args = array()) {
+    SB_Theme::the_paginate($args);
+}
+
+function sb_theme_comment_template() {
+    SB_Theme::the_comment_template();
+}
+
+function sb_theme_frontend_language($locale) {
+    if(!is_admin()) {
+        $locale = SB_Option::get_default_language();
+        return $locale;
+    }
+}
+add_filter('locale', 'sb_theme_frontend_language', 1, 1);
+
+function sb_theme_backend_language($locale) {
+    if(is_admin()) {
+        return 'en';
+    }
+}
+add_filter('locale', 'sb_theme_backend_language');
 
 require SB_THEME_INC_PATH . '/sb-theme-load.php';
