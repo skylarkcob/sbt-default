ed4f0b54d9850159c1c3c26fde37fd4072f06eb3
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2014-11-26 11:47:25 +0700

    Support mobile style for theme

--- sb-theme-custom/inc/sb-theme-custom-functions.php
+++ sb-theme-custom/inc/sb-theme-custom-functions.php
@@ -1,4 +1,12 @@
 <?php
+function sb_theme_custom_style_and_script() {
+	SB_Theme::enqueue_custom_style('sb-theme-custom-style', 'sb-theme-custom-style');
+	SB_Theme::enqueue_custom_script('sb-theme-custom', 'sb-theme-custom-script');
+	if(SB_Option::utility_enabled('responsive')) {
+		SB_Theme::enqueue_custom_responsive_style('sb-theme-custom-mobile', 'sb-theme-custom-mobile-style');
+	}
+}
+add_action('wp_enqueue_scripts', 'sb_theme_custom_style_and_script');
 
 // Stop edit from here
 require SB_THEME_CUSTOM_INC_PATH . '/sb-theme-custom-load.php';
