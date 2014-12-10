cf5be54a99fd2d17a0a2c81f48f45515b39457e5
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2014-12-10 23:05:15 +0700

    Update new theme default

--- header.php
+++ header.php
@@ -1,2 +1,4 @@
 <?php
+sb_theme_init();
 SB_Theme::the_header();
+sb_get_custom_content('content-header');
--- sb-theme-custom/inc/sb-theme-custom-load.php
+++ sb-theme-custom/inc/sb-theme-custom-load.php
@@ -2,3 +2,7 @@
 require SB_THEME_CUSTOM_INC_PATH . '/sb-theme-custom-admin.php';
 
 require SB_THEME_CUSTOM_INC_PATH . '/sb-theme-custom-ajax.php';
+
+require SB_THEME_CUSTOM_INC_PATH . '/sb-theme-custom-metas.php';
+
+require SB_THEME_CUSTOM_INC_PATH . '/sb-theme-custom-post-types-and-taxonomies.php';
