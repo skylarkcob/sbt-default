9255024e1ebd28d9034b57fb246b00d945ee4a76
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2014-12-22 20:17:22 +0700

    Update new SBT Default

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+add_filter('sb_theme_testing', '__return_true');
 require get_template_directory() . '/load.php';
 
 // Always put your code below this line
--- sb-theme-custom/inc/sb-theme-custom-functions.php
+++ sb-theme-custom/inc/sb-theme-custom-functions.php
@@ -1,10 +1,18 @@
 <?php
 function sb_theme_custom_style_and_script() {
-    SB_Theme::enqueue_custom_style('sb-theme-custom-style', 'sb-theme-custom-style');
-    if(SB_Option::responsive_enabled()) {
-        SB_Theme::enqueue_custom_responsive_style('sb-theme-custom-mobile-style', 'sb-theme-custom-mobile-style');
+    if(sb_theme_testing()) {
+        SB_Theme::enqueue_custom_style('sb-theme-custom-style', 'sb-theme-custom-style');
+        if(SB_Option::responsive_enabled()) {
+            SB_Theme::enqueue_custom_responsive_style('sb-theme-custom-mobile-style', 'sb-theme-custom-mobile-style');
+        }
+        SB_Theme::enqueue_custom_script('sb-theme-custom', 'sb-theme-custom-script');
+    } else {
+        SB_Theme::enqueue_custom_style('sb-theme-custom-style', 'sb-theme-custom-style.min');
+        if(SB_Option::responsive_enabled()) {
+            SB_Theme::enqueue_custom_responsive_style('sb-theme-custom-mobile-style', 'sb-theme-custom-mobile-style.min');
+        }
+        SB_Theme::enqueue_custom_script('sb-theme-custom', 'sb-theme-custom-script.min');
     }
-    SB_Theme::enqueue_custom_script('sb-theme-custom', 'sb-theme-custom-script');
 }
 add_action('wp_enqueue_scripts', 'sb_theme_custom_style_and_script');
 
