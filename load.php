31cfaa5b54e9298bb1e1b6fc43370952af749d78
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2015-04-13 08:44:35 +0700

    Update SBT Default 2.0.1

--- load.php
+++ load.php
@@ -2,45 +2,63 @@
 // Get sb-theme.php file path
 $sb_theme = get_template_directory() . '/sb-theme/sb-theme.php';
 
-function sb_check_theme_core_admin_notices() {
-	unset( $_GET['activated'] );
-	$my_theme = wp_get_theme();
-	$theme_name = $my_theme->get( 'Name' );
-	printf( '<div class="error"><p><strong>' . __( 'Error', 'sb-theme' ) . ':</strong> ' . __( 'The theme with name %1$s will be deactivated because of missing %2$s core package.', 'sb-theme' ) . '.</p></div>', '<strong>' . $theme_name . '</strong>', sprintf( '<a target="_blank" href="%s" style="text-decoration: none">SB Theme</a>', 'http://github.com/skylarkcob/sb-theme' ) );
-	$themes = wp_get_themes();
-	$wp_theme = '';
-	foreach ( $themes as $theme ) {
-		$author_uri = $theme->get( 'AuthorURI' );
-		if ( false !== strrpos( $author_uri, 'wordpress.org') ) {
-			$wp_theme = $theme;
-			break;
-		}
-	}
-	if ( empty( $wp_theme ) ) {
-		foreach ( $themes as $theme ) {
-			$text_domain = $theme->get( 'TextDomain' );
-			if ( false === strrpos( $text_domain, 'sb-theme') ) {
-				$wp_theme = $theme;
-				break;
-			}
-		}
-	}
-	if ( ! empty( $wp_theme ) ) {
-		switch_theme( $theme->get( 'TextDomain' ) );
-	}
+function sb_theme_missing_core_message() {
+    $my_theme = wp_get_theme();
+    $theme_name = $my_theme->get('Name');
+    return sprintf('<div class="error"><p><strong>' . __('Error', 'sb-theme') . ':</strong> ' . __('The theme with name %1$s can\'t work properly because of missing %2$s core package', 'sb-theme') . '.</p></div>', '<strong>' . $theme_name . '</strong>', sprintf('<a target="_blank" href="%s" style="text-decoration: none">SB Theme</a>', 'http://github.com/skylarkcob/sb-theme'));
+}
+
+function sb_theme_get_default_wordpress_theme_name() {
+    $transient_name = 'sb_default_wordpress_theme';
+    if(false === ($default_wordpress_theme = get_transient($transient_name))) {
+        $themes = wp_get_themes();
+        $wp_theme = '';
+        foreach($themes as $theme) {
+            $author_uri = $theme->get('AuthorURI');
+            if(strrpos($author_uri, 'wordpress.org') !== false) {
+                $wp_theme = $theme;
+                break;
+            }
+        }
+        if(empty($wp_theme)) {
+            foreach($themes as $theme) {
+                $text_domain = $theme->get('TextDomain');
+                if(strrpos($text_domain, 'sb-theme') === false) {
+                    $wp_theme = $theme;
+                    break;
+                }
+            }
+        }
+        if(!empty($wp_theme)) {
+            $default_wordpress_theme = $theme->get('TextDomain');
+            set_transient($transient_name, $default_wordpress_theme, DAY_IN_SECONDS);
+        }
+    }
+    return $default_wordpress_theme;
+}
+
+function sb_theme_switch_to_default_theme() {
+    $default_wordpress_theme = sb_theme_get_default_wordpress_theme_name();
+    if(!empty($default_wordpress_theme)) {
+        switch_theme($default_wordpress_theme);
+    }
+}
+
+function sb_theme_switch_to_default_wordpress_theme() {
+    unset($_GET['activated']);
+    echo sb_theme_missing_core_message();
+    sb_theme_switch_to_default_theme();
 }
 
 // Check if sb-theme.php file exists
-if ( ! file_exists( $sb_theme ) ) {
-	if ( ! is_admin() ) {
-		wp_die( sprintf( __( 'You must put %1$s on your current theme to use it! Click here to %2$s.', 'sb-theme' ), '<strong>SB Theme Core</strong>', sprintf( '<a target="_blank" href="%1$s">%2$s</a>', 'https://github.com/skylarkcob/sb-theme/', __( 'download', 'sb-theme' ) ) ) );
-		exit;
-	} else {
-		if ( ! empty( $GLOBALS['pagenow'] ) && 'themes.php' === $GLOBALS['pagenow'] ) {
-			add_action( 'admin_notices', 'sb_check_theme_core_admin_notices', 0 );
-		}
-	}
-	return;
+if(!file_exists($sb_theme)) {
+    if(!is_admin()) {
+        wp_die( sb_theme_missing_core_message() );
+        exit;
+    } else {
+        add_action('admin_notices', 'sb_theme_switch_to_default_wordpress_theme', 0);
+    }
+    return;
 }
 
 // Load SB Theme Core
--- sb-theme-custom/inc/sb-theme-custom-hook.php
+++ sb-theme-custom/inc/sb-theme-custom-hook.php
@@ -1,9 +1,7 @@
 <?php
 function sb_theme_custom_style_and_script() {
     SB_Theme::enqueue_custom_style( 'sb-theme-custom-style', 'sb-theme-custom-style' );
-    if ( SB_Option::responsive_enabled() ) {
-        SB_Theme::enqueue_custom_responsive_style( 'sb-theme-custom-mobile-style', 'sb-theme-custom-mobile-style' );
-    }
+    SB_Theme::enqueue_custom_responsive_style( 'sb-theme-custom-mobile-style', 'sb-theme-custom-mobile-style' );
     SB_Theme::enqueue_custom_script( 'sb-theme-custom', 'sb-theme-custom-script' );
     wp_enqueue_style( 'sb-theme-user-custom-style', get_template_directory_uri() . '/custom-style.css' );
 }
