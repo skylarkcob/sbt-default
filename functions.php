5b1fc26d48b97ae74984eb886536d5b5327c1f39
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2015-04-15 14:01:09 +0700

    Update SBT Default 2.0.4

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+define('SB_THEME_USE_OLD_VERSION', false);
 require get_template_directory() . '/load.php';
 
 // Always put your code below this line
--- sb-theme-custom/inc/sb-theme-custom-hook.php
+++ sb-theme-custom/inc/sb-theme-custom-hook.php
@@ -1,5 +1,8 @@
 <?php
 function sb_theme_custom_style_and_script() {
+    if(!sb_theme_use_old_version()) {
+        SB_Lib::load_bootstrap_and_font_awesome();
+    }
     SB_Theme::enqueue_custom_style( 'sb-theme-custom-style', 'sb-theme-custom-style' );
     SB_Theme::enqueue_custom_responsive_style( 'sb-theme-custom-mobile-style', 'sb-theme-custom-mobile-style' );
     SB_Theme::enqueue_custom_script( 'sb-theme-custom', 'sb-theme-custom-script' );
