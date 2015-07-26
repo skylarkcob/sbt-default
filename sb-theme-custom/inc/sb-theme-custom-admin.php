68186b34b0ec0916d205412e5fb19d366f8e03dc
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2015-07-26 19:04:09 +0700

    Update version 2.1.4

--- sb-theme-custom/inc/sb-theme-custom-admin.php
+++ sb-theme-custom/inc/sb-theme-custom-admin.php
@@ -1,2 +1,7 @@
 <?php
 defined('ABSPATH') or die('Please do not pip me!');
+
+function sb_theme_custom_add_admin_setting_field() {
+
+}
+add_action('sb_theme_setting_field', 'sb_theme_custom_add_admin_setting_field');
---
+++ sb-theme-custom/inc/sb-theme-custom-filter-utilities.php
@@ -0,0 +1,4 @@
+<?php
+defined('ABSPATH') or die('Please do not pip me!');
+
+add_filter('sb_theme_developing', '__return_true');
