f957a5171478f1756eac7314ea608d661f72999f
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2015-08-12 22:24:56 +0700

    Update version 2.1.8

--- load.php
+++ load.php
@@ -1,10 +1,48 @@
 <?php
 defined('ABSPATH') or die('Please do not pip me!');
 
+function sb_theme_get_language() {
+    global $sb_theme;
+    if(!is_array($sb_theme)) {
+        $sb_theme = array();
+    }
+    $sb_theme_language = isset($sb_theme['language']) ? $sb_theme['language'] : 'vi';
+    $sb_theme_language = apply_filters('sb_theme_language', $sb_theme_language);
+    $sb_theme['language'] = $sb_theme_language;
+    return $sb_theme_language;
+}
+
+$sb_theme_language = sb_theme_get_language();
+
 define('SB_THEME_WP_PHP_VERSION', '5.2.4');
 
+function sb_theme_php_version_error_message() {
+    $sb_theme_language = sb_theme_get_language();
+    $message = '<p style="font-family: arial"><strong>';
+    $message .= __('PHP version error:', 'sb-theme');
+    $message .= '</strong> ';
+    $message .= sprintf(__('Your hosting must be support at least %1$s or later for WordPress works fine, encourage use %2$s or later.', 'sb-theme'), '<strong>' . sprintf(__('PHP %s version', 'sb-theme'), SB_THEME_WP_PHP_VERSION) . '</strong>', '<strong>' . sprintf(__('PHP %s version', 'sb-theme'), '5.4') . '</strong>');
+    $message .= '</p>';
+    if('vi' == $sb_theme_language) {
+        $message = '<p style="font-family: arial"><strong>Lỗi phiên bản PHP:</strong> Hosting của bạn phải chạy <strong>PHP phiên bản ' . SB_THEME_WP_PHP_VERSION . '</strong> trở về sau để WordPress có thể hoạt động được, khuyến khích nên dùng <strong>PHP phiên bản 5.4</strong> trở lên.</p>';
+    }
+    return $message;
+}
+
+function sb_theme_php_version_error_admin_notices() {
+    echo '<div class="error">' . sb_theme_php_version_error_message() . '</div>';
+}
+
 if(version_compare(PHP_VERSION, SB_THEME_WP_PHP_VERSION, '<')) {
-    wp_die('<p style="font-family: arial"><strong>Lỗi phiên bản PHP:</strong> Hosting của bạn phải chạy <strong>PHP phiên bản ' . SB_THEME_WP_PHP_VERSION . '</strong> trở về sau để WordPress có thể hoạt động được, khuyến khích nên dùng <strong>PHP phiên bản 5.4</strong> trở lên.</p>', 'Lỗi server');
+    if(is_admin()) {
+        add_action('admin_notices', 'sb_theme_php_version_error_admin_notices');
+    } else {
+        if('vi' == $sb_theme_language) {
+            wp_die(sb_theme_php_version_error_message(), 'Lỗi server');
+        } else {
+            wp_die(sb_theme_php_version_error_message(), 'Server error');
+        }
+    }
 }
 
 define('SB_THEME_GITHUB_URL', 'http://github.com/skylarkcob/sb-theme');
@@ -20,13 +58,18 @@ function sb_theme_use_old_version() {
 // Get sb-theme.php file path
 $sb_theme = get_template_directory() . '/sb-theme/sb-theme.php';
 
-if(sb_theme_use_old_version()) {
-
-    function sb_theme_missing_core_message() {
-        $my_theme = wp_get_theme();
-        $theme_name = $my_theme->get('Name');
-        return sprintf('<div class="error"><p><strong>' . __('Lỗi:', 'sb-theme') . '</strong> ' . __('Giao diện với tên %1$s sẽ không hoạt động vì thiếu gói %2$s.', 'sb-theme') . '.</p></div>', '<strong>' . $theme_name . '</strong>', sprintf('<a target="_blank" href="%s" style="text-decoration: none">SB Theme</a>', SB_THEME_GITHUB_URL));
+function sb_theme_missing_core_message() {
+    $sb_theme_language = sb_theme_get_language();
+    $my_theme = wp_get_theme();
+    $theme_name = $my_theme->get('Name');
+    $message = sprintf('<div class="error"><p><strong>' . __('Error:', 'sb-theme') . '</strong> ' . __('%1$s theme will be deactivated because of missing %2$s.', 'sb-theme') . '</p></div>', '<strong>' . $theme_name . '</strong>', sprintf('<a target="_blank" href="%s" style="text-decoration: none">SB Theme</a>', SB_THEME_GITHUB_URL));
+    if('vi' == $sb_theme_language) {
+        $message = sprintf('<div class="error"><p><strong>Lỗi:</strong> Giao diện với tên %1$s sẽ không hoạt động vì thiếu gói %2$s.</p></div>', '<strong>' . $theme_name . '</strong>', sprintf('<a target="_blank" href="%s" style="text-decoration: none">SB Theme</a>', SB_THEME_GITHUB_URL));
     }
+    return $message;
+}
+
+if(sb_theme_use_old_version()) {
 
     function sb_theme_get_default_wordpress_theme_name() {
         $transient_name = 'sb_theme_default_wordpress_theme_name';
@@ -86,9 +129,7 @@ if(sb_theme_use_old_version()) {
 } else {
     function sb_check_theme_core_admin_notices() {
         unset($_GET['activated']);
-        $my_theme = wp_get_theme();
-        $theme_name = $my_theme->get('Name');
-        printf('<div class="error"><p><strong>' . __('Lỗi:', 'sb-theme') . '</strong> ' . __('Giao diện với tên %1$s sẽ không hoạt động vì thiếu %2$s.', 'sb-theme') . '</p></div>', '<strong>' . $theme_name . '</strong>', sprintf('<a target="_blank" href="%s" style="text-decoration: none">SB Theme</a>', SB_THEME_GITHUB_URL));
+        echo sb_theme_missing_core_message();
         $themes = wp_get_themes();
         $wp_theme = '';
         foreach($themes as $theme) {
@@ -115,7 +156,7 @@ if(sb_theme_use_old_version()) {
     // Check if sb-theme.php file exists
     if(!file_exists($sb_theme)) {
         if(!is_admin()) {
-            wp_die('<strong>' . __('Chú ý:', 'sb-theme') . '</strong>' . sprintf(__('Bạn phải sử dụng gói %1$s để giao diện được hoạt động! Nhấn vào đây để %2$s.', 'sb-theme'), '<strong>SB Theme Core</strong>', sprintf('<a target="_blank" href="%1$s">%2$s</a>', SB_THEME_GITHUB_URL, __('download', 'sb-theme'))));
+            wp_die(sb_theme_missing_core_message());
             exit;
         } elseif(!empty($GLOBALS['pagenow']) && 'themes.php' === $GLOBALS['pagenow']) {
             add_action('admin_notices', 'sb_check_theme_core_admin_notices', 0);
@@ -125,7 +166,11 @@ if(sb_theme_use_old_version()) {
 
     // Check file contain license key
     if(!file_exists(get_template_directory() . '/license.php')) {
-        wp_die(__('<span style="font-family: Tahoma; line-height: 25px;"><strong>Lỗi</strong>: Giao diện <strong>' . $theme->get('Name') . '</strong> mà bạn đang sử dụng chưa được mua bản quyền, xin vui lòng liên hệ với <strong>SB Team</strong> thông qua địa chỉ email <strong>codewpvn@gmail.com</strong> để biết thêm thông tin chi tiết.</span>', 'sb-theme'), 'Giao diện chưa mua bản quyền');
+        if('vi' == $sb_theme_language) {
+            wp_die('<span style="font-family: Tahoma; line-height: 25px;"><strong>Lỗi:</strong> Giao diện <strong>' . $theme->get('Name') . '</strong> mà bạn đang sử dụng chưa được mua bản quyền, xin vui lòng liên hệ với <strong>SB Team</strong> thông qua địa chỉ email <strong>codewpvn@gmail.com</strong> để biết thêm thông tin chi tiết.</span>', 'Giao diện chưa mua bản quyền');
+        } else {
+            wp_die(__('<span style="font-family: Tahoma; line-height: 25px;"><strong>Error:</strong> <strong>' . $theme->get('Name') . '</strong> theme that you are using is not licensed, please contact <strong>SB Team</strong> via email address <strong>codewpvn@gmail.com</strong> for more information.</span>', 'sb-theme'), 'Theme is not licensed');
+        }
         exit;
     }
 
--- single.php
+++ single.php
@@ -2,5 +2,10 @@
 defined('ABSPATH') or die('Please do not pip me!');
 
 get_header();
-SB_Theme::get_custom_content( 'content-single' );
+if(have_posts()) {
+    while(have_posts()) {
+        the_post();
+        SB_Theme::get_custom_content( 'content-single' );
+    }
+}
 get_footer();
